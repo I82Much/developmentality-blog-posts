3ba26a6c13b697822cbfb76c2d680e409140b68b
Nick Dunn <nicholas.c.dunn@gmail.com>
2012-03-12 22:50:02 -0700

    Comments

--- activity_calendar/activity_calendar.py
+++ activity_calendar/activity_calendar.py
@@ -5,7 +5,7 @@ import datetime
 from collections import defaultdict
 # 2012-03-12
 # Simple script which takes a list of activities, one per line, and generates an output csv file suitable
-# for importing into Google Docs.
+# for importing into Google Calendar.
 # This output csv contains one column for date and one for the description of the activity
 # This script outputs to standard out; output can easily be redirected to a csv file as necessary
 # Google Calendar CSV format described at http://support.google.com/calendar/bin/answer.py?hl=en&answer=45656
@@ -28,13 +28,16 @@ def main():
   december_31_ordinal = december_31.toordinal()
   
   days_of_year = [datetime.date.fromordinal(x) for x in range(january_1_ordinal, december_31_ordinal + 1)]
+  # Group the dates by month
   months = defaultdict(list)
   for date in days_of_year:
     months[date.month].append(date)
   
+  # A flattened mapping from date to activity
   date_to_activity_dict = {}
   for month in months:
     dates = months[month]
+    # creates a list of (date, activity description) pairs
     monthly_activities = zip(dates, random.sample(activities, len(dates)))
     for date, activity in monthly_activities:
       date_to_activity_dict[date] = activity
@@ -46,7 +49,6 @@ def main():
     # Google Docs needs the date in mm/dd/yy format
     date_string = date.strftime("%m/%d/%y") 
     writer.writerow([activity, date_string])
-  
   
 if __name__ == '__main__':
   main()
