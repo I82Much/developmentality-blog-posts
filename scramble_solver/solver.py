0081b81031c4192d72b9cb425f27f78cf60149ef
Nick Dunn <nicholas.c.dunn@gmail.com>
2012-05-19 22:51:34 -0700

    Reorganizing

--- scramble_solver/solver.py
+++ scramble_solver/solver.py
@@ -1,14 +1,16 @@
-import pickle
 import random
 import string
 import sys
 
+import trie
+
 """
-Solves boggle/scramble with friends
+Solves Boggle/Scramble with Friends.
 """
 
-# q doesn't stand by itself since that would not make for a fun game
 TOKENS = ['a','b','c','d','e','f','g','h','j','k','l','m','n','o','p','qu','r','s','t','u','v','w','x','y','z']
+
+DEBUG = False
 
 class Location(object):
   def __init__(self, row, col):
@@ -25,7 +27,7 @@ class Location(object):
     return '(%d, %d)' %(self.row, self.col)
   
   def __repr__(self):
-    return '(%d, %d)' %(self.row, self.col)
+    return str(self)
   
   def Adjacent(self):
     """Returns the 8 adjacent locations to this one."""
@@ -33,13 +35,18 @@ class Location(object):
     for row_offset in [-1, 0, 1]:
       for col_offset in [-1, 0, 1]:
         # 0, 0 would be the current location and that's not adjacent
-        if not (row_offset == 0 and col_offset == 0):
+        if row_offset != 0 or col_offset != 0:
           locs.append(Location(self.row + row_offset, self.col + col_offset))
     assert len(locs) == 8
     return locs
 
 class FoundWord(object):
-  """Represents a discovered word on the board, including the path taken to create it"""
+  """Represents a discovered word on the board, including the path taken to create it.
+  
+  Attributes:
+    word - the word that was found, as a string
+    locations - a list of Location objects; the path needed to reconstruct this word
+  """
 
   def __init__(self, word, locations):
     """Initialize object with given string and locations iterable."""
@@ -49,7 +56,9 @@ class FoundWord(object):
   def __repr__(self):
     return "%s: %s" %(self.word, self.locations)
 
+# TODO(ndunn): Is this board class even necessary
 class Board(object):
+  """Represents a board, a two dimensional grid of tiles."""
   def __init__(self, num_rows=4, num_cols=4):
     self.num_rows = num_rows
     self.num_cols = num_cols
@@ -73,26 +82,25 @@ class Board(object):
     return row_valid and col_valid
 
 class BoardSolver(object):
-  def __init__(self, board, dictionary):
+  def __init__(self, board, valid_words):
     self.board = board
-    self.dictionary = dictionary
+    self.valid_words = valid_words
 
-    self.trie = Trie()
-    for index, word in enumerate(dictionary):
-      # need 1 based values because 0 is False
+    self.trie = trie.Trie()
+    for index, word in enumerate(valid_words):
+      # 0 evaluates to False which screws up trie lookups; ensure value is 'truthy'.
       self.trie.add(word, index+1)
 
-
-  def HasPrefix(self, prefix):
+  def HasPrefix(self, prefix)
     return len(self.trie.find_prefix_matches(prefix))
     # Naiive implementation
-    #for word in self.dictionary:
+    #for word in self.valid_words:
     #  if word.startswith(prefix):
     #    return True
     #return False
 
   def HasWord(self, word):
-    return word in self.dictionary
+    return word in self.valid_words
 
   def Solve(self):
     """Returns a list of FoundWord objects."""
@@ -103,12 +111,11 @@ class BoardSolver(object):
       for col in range(self.board.num_cols):
         loc = Location(row, col)
         previous_locs = []
-        loc_dict = {}
-        solutions.extend(self.DoSolve(previous_locs, loc_dict, loc, ''))
+        solutions.extend(self.DoSolve(previous_locs, loc, ''))
 
     return solutions
 
-  def DoSolve(self, previous_locations, loc_dict, location, word_prefix):
+  def DoSolve(self, previous_locations, location, word_prefix):
     """Returns iterable of FoundWord objects.
     
     Args:
@@ -121,30 +128,29 @@ class BoardSolver(object):
     new_word = word_prefix + self.board[location.row][location.col]
     previous_locations.append(location)
     
-    loc_dict_copy = dict(loc_dict)
-    loc_dict_copy[location] = True
-
-    # if not self.HasPrefix(new_word):
-    #   print 'No words found starting with "%s"' %(new_word)
-    #   return solutions
-    # 
-    # if self.HasWord(new_word):
-    #   solutions.append( (new_word, previous_locations) )
-    #print previous_locations
-    solutions.append( (new_word, previous_locations) )
+    if not self.HasPrefix(new_word):
+      print 'No words found starting with "%s"' %(new_word)
+      return solutions
+    
+    # This is a valid, complete words.
+    if self.HasWord(new_word):
+      solutions.append( (new_word, previous_locations) )
 
     # Recursively search all adjacent tiles
     for new_loc in location.Adjacent():
-      if self.board.IsValidLocation(new_loc) and new_loc not in loc_dict_copy:
+      if self.board.IsValidLocation(new_loc) and new_loc not in previous_locations:
         # make a copy of the previous locations list so it is not affected by this recursive call.
-        solutions.extend(self.DoSolve(list(previous_locations), loc_dict_copy, new_loc, new_word))
+        defensive_copy = list(previous_locations)
+        solutions.extend(self.DoSolve(defensive_copy, new_loc, new_word))
       else:
-        #print 'Illegal move for %s %s %s' %(board_copy, Location(location.row + direction.row_offset, location.col + direction.col_offset), new_word)
-        pass
+        if DEBUG:
+          print 'Ignoring %s as it is invalid or already used.' %(new_loc)
 
     return solutions
 
+
 def ReadDictionary(path):
+  """Returns a set of words found in the file indicated by 'path'."""
   words = set([])
   f = open(path, "r")
   for line in f:
@@ -152,72 +158,6 @@ def ReadDictionary(path):
   f.close()
   return words
 
-# From https://bitbucket.org/woadwarrior/trie/src/6bc187d770ba/python/trie.py
-_SENTINEL = ()
-
-class Trie(object) :
-    __slots__ = ['root']
-
-    def __init__( self ) :
-        self.root = [None,{}]
-
-    def __getstate__( self ) :
-        if any(self.root) :
-            return self.root
-        else :
-            return False
-
-    def __setstate__(self, s) :
-        self.root = s
-
-    def __contains__( self, s ) :
-        if self.find_full_match(s,_SENTINEL) is _SENTINEL :
-            return False
-        return True
-
-    def add( self, key, value ) :
-        curr_node = self.root
-        for ch in key :
-            node = curr_node[1]
-            if ch in node :
-                curr_node = node[ch]
-            else :
-                curr_node = node[ch] = [None,{}]
-        curr_node[0] = value
-
-    def _find_prefix_match( self, key ) :
-        curr_node = self.root
-        remainder = key
-        for ch in key :
-            if ch in curr_node[1] :
-                curr_node = curr_node[1][ch]
-            else :
-                break
-            remainder = remainder[1:]
-        return [curr_node,remainder]
-
-    def find_full_match( self, key, fallback=None ) :
-        '''
-        Returns the value associated with the key if found else, returns fallback
-        '''
-        r = self._find_prefix_match( key )
-        if not r[1] and r[0] :
-            return r[0][0]
-        return fallback
-
-    def find_prefix_matches( self, prefix ) :
-        l = self._find_prefix_match( prefix )
-        if l[1] :
-            return []
-        stack = [l[0]]
-        ret = []
-        while stack :
-            d = stack.pop()
-            if d[0] :
-                ret.insert(0,d[0])
-            for c in d[1] :
-                stack.append(d[1][c])
-        return ret
 
 def main():
   b = Board(4, 4)
@@ -253,7 +193,6 @@ def main():
   solutions = solver.Solve() #set(solver.Solve())
   
   print 'Found %d solutions' %len(solutions)
-  pickle.dump(solutions, output)
   # for solution in sorted(solutions):
   #     print solution
 
